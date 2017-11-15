8f2892a15dcc6049412edff4583e4fa0320cc17b
Admin <lucastimothycook@gmail.com>
2017-11-14 21:59:29 -0600

    First real commit

---
+++ MaxIndependentSets.py
@@ -0,0 +1,21 @@
+import igraph as ig
+import json
+
+def main():
+
+
+
+	N=len(data['nodes'])
+	L=len(data['links'])
+	Edges=[(data['links'][k]['source'], data['links'][k]['target']) for k in range(L)]
+
+	G=ig.Graph(Edges, directed=False)
+
+
+
+def Graph():
+
+def Colors():
+
+if __name__ == "__main__":
+    main()
